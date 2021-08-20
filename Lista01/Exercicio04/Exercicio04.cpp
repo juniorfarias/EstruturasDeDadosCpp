b9c5285550ae51baf7d378061f5163582e9f9528
José Wellington Araújo F. Júnior <wellington_farias@virgo.atlantico.net.br>
2021-08-19 23:04:13 -0300

    add Exerciocio04 da Lista 01

---
+++ Lista01/Exercicio04/Exercicio04.cpp
@@ -0,0 +1,12 @@
+// Autor: juniorfarias
+// Data: 19/08/2021
+// Objetivo: O programa abaixo está correto?
+
+void ProcessaY (int parA) {
+int d = parA * parA;
+    //return d; Uma função void não tem retorno
+}
+
+int main () {
+    //cout << ProcessaX (3,4) << endl; Aqui o erro é por conta que essa função ProcessaX não existe
+}
